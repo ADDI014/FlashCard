2ba704bfa462cb7835615d69dd84a18d37a5f9cc
ANKIT KUMAR <ankitmom0393@gmail.com>
2024-08-12 20:26:41 +0530

    Module defined

--- backend/index.js
+++ backend/index.js
@@ -4,30 +4,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const Flashcard = require('./models/Flashcard');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Correct MongoDB URI
 const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://ADDI014:Ankit123@flashcard.qvkcl.mongodb.net/?retryWrites=true&w=majority&appName=FlashCard';
 
-mongoose.connect(mongoURI)
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Failed to connect to MongoDB Atlas:', err));
+// mongoose.connect(mongoURI)
+//     .then(() => console.log('Connected to MongoDB Atlas'))
+//     .catch(err => console.error('Failed to connect to MongoDB Atlas:', err));
 
-// Define the Flashcard schema
-const flashcardSchema = new mongoose.Schema({
-    question: String,
-    answer: String,
-});
 
-// Create a model based on the schema
-const Flashcard = mongoose.model('Flashcard', flashcardSchema);
+mongoose.connect(mongoURI ,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 
 // Define routes
 app.get('/flashcards', async (req, res) => {
     try {
         const flashcards = await Flashcard.find();
+        console.log(flashcards);
         res.json(flashcards);
     } catch (err) {
         res.status(500).send(err);
@@ -44,7 +46,5 @@ app.post('/flashcards', async (req, res) => {
     }
 });
 
-// Start server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/models/Flashcard.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+const flashcardSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+const Flashcard = mongoose.model('Flashcard', flashcardSchema);
+
+module.exports = Flashcard;
